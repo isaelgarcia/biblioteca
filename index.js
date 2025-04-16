2df4b6925216d6740654f45d6fbc3d0ebfe7aef3
Isael Garcia <isaelgarcia180@gmail.com>
2025-04-15 22:17:58 -0300

    feat: frontend

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const livrosRoutes = require('./routes/livros');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static('public'));
 app.use(express.json());
 app.use('/livros', livrosRoutes);
 
